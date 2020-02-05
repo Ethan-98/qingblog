e3cfd928ea1adc1f83d8909f6b2deca876ce1d06
zakary1998 <shizexin98@gmail.com>
2020-02-05 19:09:05 +0800

    登录功能

--- src/test/java/com/zakary/qingblog/QingblogApplicationTests.java
+++ src/test/java/com/zakary/qingblog/QingblogApplicationTests.java
@@ -1,13 +1,34 @@
 package com.zakary.qingblog;
 
+import com.zakary.qingblog.domain.User;
+import com.zakary.qingblog.service.LoginService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
 class QingblogApplicationTests {
 
+    @Autowired
+    private LoginService loginService;
+
+    private Logger logger;
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+    }
+
+    @Test
+    public void loginTest1(){
+        User user=new User();
+        user.setUserName("test");
+        user.setUserPassword("12345678");
+        loginService.login(user);
     }
 
 }
