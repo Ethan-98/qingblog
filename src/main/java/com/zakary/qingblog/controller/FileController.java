9b5929cb29f6a3ab073a76648af61505ee2d4a65
Ethan-98 <shizexin98@gmail.com>
2020-02-11 17:50:10 +0800

    test push

--- src/main/java/com/zakary/qingblog/controller/FileController.java
+++ src/main/java/com/zakary/qingblog/controller/FileController.java
@@ -50,6 +50,7 @@ public class FileController {
         File localFile = new File("D:\\OneDrive\\Code\\IDEA\\qingblog\\src\\main\\resources\\static\\image\\profileImg", newFileName);
         file.transferTo(localFile);
         logger.info(localFile.getAbsolutePath());
-        return new FileInfo(1, "上传成功", request.getRequestURL().substring(0,request.getRequestURL().lastIndexOf(\"/\"))+\"/upload/\"+newFileName);
+        return null;
+//        return new FileInfo(1, "上传成功", request.getRequestURL().substring(0,request.getRequestURL().lastIndexOf("/"))+"/upload/"+newFileName);
     }
 }
